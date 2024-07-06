128da8fd09374723c314f89e8ed1a3f0905d9bc9
soph-ya101010 <papayalimernh321@gmail.com>
2024-07-06 11:37:45 +0800

    Update ContentView.swift

--- joke app/ContentView.swift
+++ joke app/ContentView.swift
@@ -19,8 +19,8 @@ struct ContentView: View {
                       punchline:"There was nothing left but de Brie"),
                  Joke(setup: "Dad, can you put my shoes on?",
                       punchline: "I don't think they'll fit me"),
-                Joke(setup: "What did the sea say to the seashore?",
-                    punchline: "Nothing, it just waved!"]
+                 Joke(setup: "What did the sea say to the seashore?",
+                    punchline: "Nothing, it just waved!")]
     
     @State private var showPunchline = false
     @State private var currentJokeIndex = 0
